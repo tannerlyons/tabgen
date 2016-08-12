3d17c69cfd0e61588907fa0b045ddde0e076e764
Tanner Means <tanner.means@gmail.com>
2016-08-12 00:10:23 -0700

    added favicon, suggestion ticker

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const throttle = require('lodash.throttle');
+const shuffle = require('shuffle-array');
 
 // Ensure our CSS is loaded.
 require('../styles/app.less');
@@ -12,12 +13,43 @@ const FAVICON_ID = '#favicon';
 
 // Listen for tab name changes
 document.querySelector(TAB_NAME_ID).addEventListener('input', (elem) => {
-  let newText = elem.target.value;
+  const newText = elem.target.value;
   document.title = newText;
 });
 
 // Listen for tab icon changes
 document.querySelector(TAB_ICON_ID).addEventListener('input', throttle((elem) => {
-  let newSrc = elem.target.value;
+  const newSrc = elem.target.value;
   document.querySelector(FAVICON_ID).href = newSrc;
 }, 500));
+
+
+const VERB_ID = '#verb';
+const NOUN_ID = '#noun';
+const SCROLL_RATE = 3000;
+
+const verbs = ['Impress', 'Astound', 'Scare', 'Worry', 'Make fun of',
+  'Insult', 'Compete with', 'Disgust', 'Prank', 'Misdirect', 'Confuse', 'Wow'];
+shuffle(verbs);
+
+const nouns = ['friends', 'loved ones', 'coworkers', 'boss', 'relatives', 'blind-dates',
+  'tinder matches', 'IT department', 'book club', 'dog', 'wife', 'husband', 'partner',
+  'future self'].map(term=>term +'.');
+shuffle(nouns);
+
+// Update the verb and noun every once in a while;
+function scrollThrough( domId, words ) {
+  const element = document.querySelector(domId);
+  let currentIdx = 0;
+  let update = () => {
+    element.innerHTML = words[currentIdx];
+    currentIdx++;
+    if (currentIdx === words.length) {
+      currentIdx = 0;
+    }
+  };
+  setInterval(update, SCROLL_RATE);
+}
+
+scrollThrough(VERB_ID, verbs);
+scrollThrough(NOUN_ID, nouns);
